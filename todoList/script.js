01c8b453a82a7547bff6aaa74130fb166413be1a
TsengelBair <129286264+TsengelBair@users.noreply.github.com>
2023-05-19 22:43:05 +0300

    Update script.js

--- todoList/script.js
+++ todoList/script.js
@@ -3,15 +3,15 @@ document
   .addEventListener("submit", function (event) {
     event.preventDefault();
 
-    var todoInput = document.getElementById("todo-input");
-    var todoList = document.getElementById("todo-list");
+    let todoInput = document.getElementById("todo-input");
+    let todoList = document.getElementById("todo-list");
 
     if (todoInput.value.trim() === "") {
       alert("Please enter a task.");
       return;
     }
 
-    var task = document.createElement("li");
+    let task = document.createElement("li");
     task.innerText = todoInput.value;
     task.addEventListener("click", function () {
       this.classList.toggle("done");
